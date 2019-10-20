f0fa1c277c1aebf29dd1ae065bef31bca0b3fc62
Sam Dawson <sam.dawson181@gmail.com>
2019-10-20 18:34:08 +1100

    Revert "Add using background"
    This reverts commit 71959ffc9e2ca70885fa3c3851dd9f43da28db7b.

--- static/background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  chrome.storage.sync.get("websites", function({ websites = [] }) {
-    websites.forEach(({ name, count }) => {
-      if (tab.url.includes(name)) {
-        chrome.tabs.executeScript(tabId, { file: "content.js" });
-      }
-    });
-  });
-});
